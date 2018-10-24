f1b516a34fc417902d242d32e2f951d9cfc5c67b
Raji Rustamov <racirustemov@gmail.com>
2018-10-24 00:37:27 -0700

    indention

--- app/api.js
+++ app/api.js
@@ -3,64 +3,60 @@ var Converter = require("csvtojson").Converter;
 
 // Check price for given form inputs
 exports.checkPrice = function (req, res){
+    // request parameters
+    var bed = req.body.bedroom;
+    var bath = req.body.bathroom;
 
-	// request parameters
-	var bed = req.body.bedroom;
-	var bath = req.body.bathroom;
+    // hash tables
+    var price = {};
+    var count = {};
+    var avg_price = {};
+    var key = "";
 
-	// hash tables
-	var price = {};
-	var count = {};
-	var avg_price = {};
-	var key ="";
-
-	// retrieve data from csv file
-	var converter = new Converter({});
+    // retrieve data from csv file
+    var converter = new Converter({});
+    
     converter.fromFile("./data/rent_data.csv",function(err, result){
+        // if there is an error retrieving
+        if (err)
+            return err;
 
-		// if there is an error retrieving
-		if (err)
-			return err;
+        // Compare request data with CSV file data
+        res.json(compare(bed, bath, result)); 
+    });
 
-		// Compare request data with CSV file data
-		res.json(compare(bed, bath, result)); 
-	});
+    // Compares request data with exist data
+    function compare(bed, bath, result) {
+        // create key for hash
+        var key = bed + '-' + bath;
 
-	// Compares request data with exist data
-	function compare(bed, bath, result){
+        // retrieve data from hash table
+        var hasdData = hashingCsvData(result);
 
-		// create key for hash
-		var key = bed + '-' + bath;
+        // if request data exist in hash table, then return it 
+        if (hasdData[key]) {
+            return hasdData[key];
+        } else {
+            return -1;
+        }
+    };
 
-		// retrieve data from hash table
-		var hasdData = hashingCsvData(result);
+    // Hashing CSV data
+    function hashingCsvData(result){
+        for(var i=0; i < result.length; i++) {
+            // create key for hashtable
+            var key = result[i].bedrooms + '-' + result[i].bathrooms;
 
-		// if request data exist in hash table, then return it 
-		if(hasdData[key]){
-			return hasdData[key];
-		}else{
-			return -1;
-		}
-	}
+            // price sum of common bedrooms and bathrooms
+            price[key] = (price[key] || 0) + result[i].price;
 
-	// Hashing CSV data
-	function hashingCsvData(result){
+            // count of common bedrooms and bathrooms
+            count[key] = (count[key] || 0) + 1; 
 
-		for(var i=0; i < result.length; i++){
-
-			// create key for hashtable
-			var key = result[i].bedrooms + '-' + result[i].bathrooms;
-
-			// price sum of common bedrooms and bathrooms
-			price[key] = (price[key] || 0) + result[i].price;
-
-			// count of common bedrooms and bathrooms
-			count[key] = (count[key] || 0) + 1; 
-
-			// average price for per common bedrooms and bathrooms sets
-			avg_price[key] = parseInt(price[key] / count[key]);
-		}
-		// Return average of prices	
-		return avg_price;
-	};		
+            // average price for per common bedrooms and bathrooms sets
+            avg_price[key] = parseInt(price[key] / count[key]);
+        }
+        // Return average of prices	
+        return avg_price;
+    };		
 }
