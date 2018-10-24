5fc72906978d6fb3a2db9e068be348f2fe2ca974
Raji Rustamov <racirustemov@gmail.com>
2018-10-24 00:33:06 -0700

    indention

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // set up ======================================================================
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var port  	 = process.env.PORT || 3000; 				// set the port
-var bodyParser = require('body-parser'); 	            // pull information from HTML POST (express4)
+var express    = require('express');
+var app        = express();                       // create our app w/ express
+var port       = process.env.PORT || 3000;        // set the port
+var bodyParser = require('body-parser');          // pull information from HTML POST (express4)
 
 // configuration ===============================================================
-app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
-app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); 									// parse application/json
+app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({'extended':'true'}));    // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                             // parse application/json
 
 // routes ======================================================================
 require('./app/routes.js')(app);
